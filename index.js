7dbe088c64b4662a589161a0e3153751f9c04449
unknown <mrehawk82@gmail.com>
2020-11-14 16:10:36 -0500

    initial program

---
+++ index.js
@@ -0,0 +1,27 @@
+let myArray = [];
+function exaggerate(incoming_word){
+	for (var i = 0; i < incoming_word.length; i++) {
+		function myFunction() {
+			var outgoing_word;
+			var focused_letter;
+			var x = Math.floor((Math.random() * 2) + 1);
+
+			if(x === 1){
+				 focused_letter = incoming_word[i].toLowerCase();
+			} else {
+				 focused_letter = incoming_word[i].toUpperCase();
+				 
+			}
+			myArray.push(focused_letter);
+if (myArray.length === incoming_word.length) {
+	outgoing_word = myArray.join("");
+
+	console.log(outgoing_word);
+}
+
+		}
+		myFunction();
+	}
+}
+
+exaggerate("Captain America");
